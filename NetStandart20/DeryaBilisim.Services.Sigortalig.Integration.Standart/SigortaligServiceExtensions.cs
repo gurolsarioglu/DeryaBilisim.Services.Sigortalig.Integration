8e67c8b3fa50c7377008a4fc96e716e172cf07ab
Murat Başeren <muratbaseren@gmail.com>
2020-09-25 13:02:53 +0300

    service extension is added

---
+++ NetStandart20/DeryaBilisim.Services.Sigortalig.Integration.Standart/SigortaligServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DeryaBilisim.Services.Sigortalig.Integration.Standart
+{
+    public static class SigortaligServiceExtensions
+    {
+        public static void AddSigortaligService(this IServiceCollection services, string endpoint, string token)
+        {
+            services.AddSingleton<SigortaligService>(x => new SigortaligService(endpoint, token));
+        }
+    }
+}

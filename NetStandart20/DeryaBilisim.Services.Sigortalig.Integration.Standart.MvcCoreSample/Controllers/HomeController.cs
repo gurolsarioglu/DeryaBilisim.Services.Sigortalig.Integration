46f4888180295c0996f29cf279dace9bafafdeca
Murat Başeren <muratbaseren@gmail.com>
2020-10-06 23:02:45 +0300

    Sample app updated

--- NetStandart20/DeryaBilisim.Services.Sigortalig.Integration.Standart.MvcCoreSample/Controllers/HomeController.cs
+++ NetStandart20/DeryaBilisim.Services.Sigortalig.Integration.Standart.MvcCoreSample/Controllers/HomeController.cs
@@ -19,7 +19,14 @@ namespace MvcCoreSample.Controllers
 
         public IActionResult Index()
         {
-            // BES
+            // Get All Insurance Type and API EndPoints
+            //(tr:Poliçe tiplerini ve API uç adreslerini verir.)
+            var getAllInsuranceTypes = _sigortaligService.GetAllInsuranceTypes();
+
+
+
+            // Sample Individual Pension Policy Request Registration 
+            // (tr:BES poliçe talebi kaydı)
             var response = _sigortaligService.NewBesInsuranceRequest(new NewBesInsuranceRequestModel
             {
                 BirthDate = DateTime.Now.AddYears(-10),
@@ -30,11 +37,29 @@ namespace MvcCoreSample.Controllers
                 TCKN = "32331135922"
             });
 
-
             Debug.WriteLine(response.StatusCode, "Sigortalig Request");
             Debug.WriteLine(response.Data?.message, "Sigortalig Request");
             Debug.WriteLine(response.Data?.data?.Message, "Sigortalig Request");
             Debug.WriteLine(response.Data?.data?.ReferanceCode, "Sigortalig Request");
+
+
+
+            // Create Other New Insurance Request.
+            // This method is used for the use of policy types that do not have any method. 
+            // (tr:Bu metot, metodu olmayan poliçe tiplerinin kullanımı içindir.)
+            var response2 = _sigortaligService.NewOtherInsuranceRequest(new NewOtherInsuranceRequestModel
+            {
+                FirstName = "Andrew",
+                LastName = "King",
+                Mail = "kau31288@eoopy.com",
+                PhoneNumber = "601-915-0841",
+                TCKN = "32331135922"
+            }, InsuranceType.AgricultureInsurance);
+
+            Debug.WriteLine(response2.StatusCode, "Sigortalig Request");
+            Debug.WriteLine(response2.Data?.message, "Sigortalig Request");
+            Debug.WriteLine(response2.Data?.data?.Message, "Sigortalig Request");
+            Debug.WriteLine(response2.Data?.data?.ReferanceCode, "Sigortalig Request");
 
             return View();
         }
